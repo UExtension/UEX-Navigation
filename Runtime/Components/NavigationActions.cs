6d8ce7f3d00873dd015a87fcddd53606553b5afd
Théo Martel <33041639+hazard4u@users.noreply.github.com>
2025-12-05 23:41:38 +0100

    refactor: update NavigationActions methods to use UniTask `.Forget()` for async calls and add `SetTab` and `Reload` methods
    (cherry picked from commit 7344473eea2b70623190fd69158dd2ccfbdb91be)

--- Runtime/Components/NavigationActions.cs
+++ Runtime/Components/NavigationActions.cs
@@ -1,4 +1,6 @@
+using Cysharp.Threading.Tasks;
 using UExtension.Navigation.RouteFactory;
+using UExtension.Navigation.RouteFactory.Tab;
 using UnityEngine;
 
 namespace UExtension.Navigation.Components
@@ -7,27 +9,37 @@ namespace UExtension.Navigation.Components
     {
         public void Push(AbstractRouteFactory route)
         {
-            _ = NavigationService.Push(route);
-        }
-
-        public void Navigate(AbstractRouteFactory route)
-        {
-            _ = NavigationService.Navigate(route);
+            NavigationService.Push(route).Forget();
         }
 
         public void Pop()
         {
-            _ = NavigationService.Pop();
+            NavigationService.Pop().Forget();
+        }
+
+        public void Navigate(AbstractRouteFactory route)
+        {
+            NavigationService.Navigate(route).Forget();
+        }
+
+        public void SetTab(TabRouteFactory tabRouteFactory, AbstractRouteFactory tabFactory)
+        {
+            NavigationService.SetTab(tabRouteFactory, tabFactory).Forget();
         }
 
         public void Replace(AbstractRouteFactory route)
         {
-            _ = NavigationService.Replace(route);
+            NavigationService.Replace(route).Forget();
         }
 
         public void Root(AbstractRouteFactory route)
         {
-            _ = NavigationService.Root(route);
+            NavigationService.Root(route).Forget();
+        }
+
+        public void Reload()
+        {
+            NavigationService.Reload().Forget();
         }
     }
 }
