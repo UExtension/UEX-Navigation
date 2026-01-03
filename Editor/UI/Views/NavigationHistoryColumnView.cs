a9cc19d6120e7457e8a941a6e8ed29edbc653d9d
Théo Martel <33041639+Hazard4U@users.noreply.github.com>
2026-01-03 21:08:45 +0100

    feat: adjust NavigationHistoryColumnView for play mode transitions and improve UI responsiveness

--- Editor/UI/Views/NavigationHistoryColumnView.cs
+++ Editor/UI/Views/NavigationHistoryColumnView.cs
@@ -1,22 +1,57 @@
-﻿using UnityEngine.UIElements;
+﻿using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace UExtension.Navigation.Editor.UI.Views
 {
     [UxmlElement]
     public partial class NavigationHistoryColumnView : VisualElement
     {
+        public SearchableRoutesListView RoutesListView { get; }
+
         public NavigationHistoryColumnView()
         {
             AddToClassList("navigation-history-column-view");
 
-            var routesListView = new SearchableRoutesListView();
-            var navigationHistory = new NavigationHistoryView();
+            RoutesListView = new SearchableRoutesListView();
+            Add(RoutesListView);
 
+            if (EditorApplication.isPlaying)
+            {
+                AddNavigationHistoryView();
+                RoutesListView.AddToClassList("searchable-routes-list-view--side");
+            }
+
+            EditorApplication.playModeStateChanged += HandlePlayModeStatedChanged;
+        }
+
+        private void HandlePlayModeStatedChanged(PlayModeStateChange obj)
+        {
+            switch (obj)
+            {
+                case PlayModeStateChange.EnteredEditMode:
+                    if (childCount > 1)
+                    {
+                        RemoveAt(1);
+                        RoutesListView.RemoveFromClassList("searchable-routes-list-view--side");
+                    }
+
+                    break;
+                case PlayModeStateChange.EnteredPlayMode:
+                    if (childCount < 2)
+                    {
+                        AddNavigationHistoryView();
+                        RoutesListView.AddToClassList("searchable-routes-list-view--side");
+                    }
+
+                    break;
+            }
+        }
+
+        private void AddNavigationHistoryView()
+        {
             var scrollView = new ScrollView();
             scrollView.AddToClassList("navigation-history-column-view__scroll-view");
-            scrollView.Add(navigationHistory);
-
-            Add(routesListView);
+            scrollView.Add(new NavigationHistoryView());
             Add(scrollView);
         }
     }
